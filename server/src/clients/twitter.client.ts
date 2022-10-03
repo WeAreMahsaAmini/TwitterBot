6cb35b48b7c8a874a025fa21978c2c71e5c9fbdd
Farnood Massoudi <fa.massoudi@gmail.com>
2022-10-02 22:15:45 -0400

    add twitter client

---
+++ server/src/clients/twitter.client.ts
@@ -0,0 +1,19 @@
+import { TWITTER_BEARER_TOKEN } from '@/config'
+import { TwitterUser } from '@/interfaces/users.interface'
+import { Client } from 'twitter-api-sdk'
+
+export class TwitterCLient {
+  private client: Client
+
+  constructor(accessToken?: string) {
+    this.client = new Client(accessToken || TWITTER_BEARER_TOKEN)
+  }
+
+  getAuthUser = async (access_token?: string): Promise<TwitterUser> => {
+    const client = access_token ? new Client(access_token) : this.client
+    const user = await client.users.findMyUser({
+      'user.fields': ['id', 'username', 'name', 'protected', 'verified'],
+    })
+    return user.data as TwitterUser
+  }
+}
