ae4a3a2f8fa71f58375f9418542c78c44135fc43
Farnood Massoudi <fa.massoudi@gmail.com>
2022-10-03 02:20:08 -0400

    add populate for liberty job to list of jobs

--- worker/src/worker.ts
+++ worker/src/worker.ts
@@ -1,4 +1,6 @@
-const AVAILABLE_JOBS = []
+import { populateForLiberty } from '@jobs/populate-for-liberty.job'
+
+const AVAILABLE_JOBS = [populateForLiberty]
 
 const job_runner = () => {
   AVAILABLE_JOBS.forEach(job => {
